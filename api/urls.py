13bcf38a7711a57cf484ac644518e711e9683674
nyzd <azerothadev@gmail.com>
2025-03-08 17:46:19 +0330

    mushaf router

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from rest_framework import routers
-from account import views
+from account import views as account_views
+from quran import views as quran_views
 from django.urls import path, include
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+router.register(r'users', account_views.UserViewSet)
+router.register(r'groups', account_views.GroupViewSet)
+router.register(r'mushafs', quran_views.MushafViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- quran/serializers.py
+++ quran/serializers.py
@@ -1,14 +1,12 @@
-from django.contrib.auth.models import Group, User
 from rest_framework import serializers
 
+from quran.models import Mushaf
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'groups']
+class MushafSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    short_name = serializers.CharField(required=True, max_length=100)
+    name = serializers.CharField(required=True)
+    source = serializers.CharField(required=True)
 
-
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ['url', 'name']
+    def create(self, validated_data):
+        return Mushaf.objects.create(**validated_data)
