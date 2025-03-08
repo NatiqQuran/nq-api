363843368d837751433e899e5082901fdf3dda4d
nyzd <azerothadev@gmail.com>
2025-03-08 16:29:18 +0330

    INIT

---
+++ api/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from rest_framework import routers
+from quran import views
+from django.urls import path, include
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls'))
+]
